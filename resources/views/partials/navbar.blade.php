6f23772bb9b76a581b54fd594a0a64088b45c608
wahyutirta <wahyutirta12345@gmail.com>
2021-10-26 00:15:14 +0800

    finish blade material

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -7,13 +7,13 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <a class="nav-link {{ ($title == "Home" ) ? 'active' : '' }}" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
+            <a class="nav-link {{ ($title == "About" ) ? 'active' : '' }}" href="/about">About</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/posts">Blog</a>
+            <a class="nav-link {{ ($title == "Posts" ) ? 'active' : '' }}"  href="/posts">Blog</a>
           </li>
           
         </ul>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.main')
 
 @section('container')
-<h1>Halaman Posts</h1>
+
+@foreach($posts as $post)
+<article class="mb-5">
+    <h2>
+        <a href="/posts/{{ $post['slug'] }}">{{ $post['title'] }}</a>
+    </h2>
+    <h2>By: {{ $post['author'] }} </h2>
+    <p> {{ $post['body'] }} </p>
+</article>
+@endforeach
+
 @endsection
