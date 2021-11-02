5425db9c4f9afef350020a49079da593ae053eb5
wahyutirta <wahyutirta12345@gmail.com>
2021-11-03 07:33:45 +0800

    route model binding

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,9 +2,10 @@
 
 @section('container')
 <article>
-    <h2>{{ $post['title'] }}</h2>
-    <h5>{{ $post['author'] }}</h5>
-    <p>{{ $post["body"] }}</p>
+    <h2>{{ $post->title }}</h2>
+    {{-- {{ $post->body }} --}}
+    {{-- tanpa escape untuk rendering tag html e.g. <p> --}}
+        {!! $post->body !!}
 </article>
 
 <a href="/posts">Back to Posts</a>
